712ec5d3290e16000f6d91ec9bbb046478d06801
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-15 01:45:22 +0530

    Fixed file typo

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-    origin : 'http://localhost:5173',
-    credentials : true,
-}))
+app.use(cors({credentials : true,}))
 
 // User routes
 app.use('/api/users', userRoutes)
