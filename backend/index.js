78add19677ac22088753c65341290b747b9bef3a
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-15 01:36:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin : 'http://localhost:5173',
+    origin : 'https://rabbit-e-commerce-frontend.vercel.app',
     credentials : true,
 }))
 
