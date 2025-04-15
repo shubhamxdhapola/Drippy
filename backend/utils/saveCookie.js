a9d70723bc5f6f1320913f67ace328b7ba4398b2
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-16 03:05:12 +0530

    Updated cookie and backend env

--- backend/utils/saveCookie.js
+++ backend/utils/saveCookie.js
@@ -1,7 +1,7 @@
 export const saveCookie = (jwtToken, res) => {
     res.cookie('jwt', jwtToken, {
         httpOnly: true,        
-        secure: true,          
+        secure: process.env.NODE_ENV === 'production',        
         sameSite: 'strict',    
         maxAge: 7 * 24 * 60 * 60 * 1000, 
     })
