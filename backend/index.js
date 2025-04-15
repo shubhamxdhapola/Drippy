c5acb9d0fa023d17d5a9546b2f97c92821baf80a
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-16 00:07:08 +0530

    New change

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin : 'http://localhost:5173',
+    origin : process.env.ORIGIN,
     credentials : true,
 }))
 
