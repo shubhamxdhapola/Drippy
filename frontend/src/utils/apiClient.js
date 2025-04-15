817b91e0d07c5d3490484a62cd8933d961c1fe54
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-16 02:55:22 +0530

    Updated frontend env

--- frontend/src/utils/apiClient.js
+++ frontend/src/utils/apiClient.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.MODE === 'development' 
-    ? import.meta.env.VITE_BACKEND_URL : '/'
+const baseURL = import.meta.env.VITE_BACKEND_URL
 
 export const apiClient = axios.create({
     baseURL,
