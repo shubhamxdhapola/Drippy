e3180c3e70de7efdc984ccb3592eed3d36be6dc1
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-22 10:26:23 +0530

    Minor fix

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -9,7 +9,7 @@ const ProductGrid = ({ products, loading, error }) => {
       {products.map((product, index) => (
         <Link key={index} to={`/product/${product._id}`} className="block">
           <div className="bg-white p-4 rounded-lg">
-            <div className="w-full h-[310px] mb-4">
+            <div className="w-full h-[350px] mb-4">
               <img
                 src={product.images[0].url}
                 alt={product.images[0].altText || product.name}
