9dd2a0b402880ced5aba9b86b37848b5d3f48d9e
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-05-29 20:45:34 +0530

    Minor change

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -156,7 +156,7 @@ const Footer = () => {
       {/* Footer Bottom */}
       <div className="container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6">
         <p className="text-gray-600 text-sm tracking-tighter text-center">
-          &copy; 2025, Drippy. All Rights Reserved.
+          &copy; 2025, Drippy. {" "} All Rights Reserved.
         </p>
       </div>
     </footer>
