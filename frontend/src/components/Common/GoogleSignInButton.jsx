3fda1b0324feeb38e52ab9e4742ae8956bc95bc8
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-05-31 16:54:20 +0530

    Minor change

--- frontend/src/components/Common/GoogleSignInButton.jsx
+++ frontend/src/components/Common/GoogleSignInButton.jsx
@@ -1,7 +1,7 @@
 const GoogleSignInButton = () => {
   return (
     <div className="flex items-center justify-center">
-      <button className=" w-full p-2 border flex justify-center items-center gap-6 font-semibold rounded-lg text-gray-800  bg-gray-200 hover:bg-gray-300 duration-300">
+      <button className=" w-full p-2 border flex justify-center items-center gap-6 font-semibold rounded-lg text-gray-800 shadow-sm bg-gray-100 hover:bg-gray-200 duration-300">
         <img
           className="w-5 h-5"
           src="https://www.svgrepo.com/show/475656/google-color.svg"
